d2222f9ce87267d66ae7045eb3bcf20209773e2c
code-leesooncheol <2sooncheol@gmail.com>
2023-02-06 17:16:23 +0900

    JS day01 study

---
+++ workspace/day01/let-const.js
@@ -0,0 +1,21 @@
+const ON =1;
+
+function testConst() {
+    // ON = 2;
+    console.log(ON);
+}
+
+
+function test() {
+    let data = 1;
+
+}
+
+function f() {
+    let data = 10;
+    // let data = 20;
+    data = 20;
+    console.log(data);
+}
+
+f();
