ecb4dc004a46cd9602ecb586e89cbc06da06d335
code-leesooncheol <2sooncheol@gmail.com>
2023-02-10 22:00:00 +0900

    JS day05 study

---
+++ workspace/day05/task01/task01.js
@@ -0,0 +1,20 @@
+// 기능체크 
+
+// 아동, 청소년, 성인 객체를 생성해 배열에 담아주기
+
+// 검색 기능
+
+// 확인 버튼을 통해 해당 행 찾기
+
+// 검색을 통한 행 백그라운드 색깔 찾기 
+
+// 입력 내용이 잘못 됐을 때 alret을 통한 메시지 출력
+
+// 입력 내용이 잘못 됐을 때 기존에 검색 됏던 행의 색깔 초기화
+
+
+const child = document.querySelector(".child");
+const teenager = document.querySelector(".teenager");
+const adult = document.querySelector(".adult");
+
+
